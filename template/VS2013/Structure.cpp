5e4575b336c1ce9f34fae84c3be906efb07df174
Wishe Deom <wishedeom@gmail.com>
2016-03-22 23:00:48 -0400

    Structure::draw() implemented.

--- template/VS2013/Structure.cpp
+++ template/VS2013/Structure.cpp
@@ -8,6 +8,7 @@
 Structure::Structure(const std::vector<glm::vec2>& baseVertices, const float height)
 {
 	std::tie(m_vertices, m_indices) = computeStructureData(baseVertices, height);
+	generateBuffers();
 }
 
 
@@ -28,7 +29,34 @@ std::pair<std::vector<glm::vec3>, std::vector<GLuint>> Structure::computeStructu
 
 void Structure::generateBuffers()
 {
+	// Generate vertex array object and vertex and element buffer objects
 	glGenVertexArrays(1, &m_VAO);
 	glGenBuffers(1, &m_VBO);
 	glGenBuffers(1, &m_EBO);
+
+	// Bind VAO
+	glBindVertexArray(m_VAO);
+
+	// Send vertex data to VAO
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices[0].x) * m_vertices.size(), &m_vertices[0].x, GL_STATIC_DRAW);
+
+	// Send vertex data to VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_indices[0]) * m_indices.size(), &m_indices[0], GL_STATIC_DRAW);
+
+	// Initialize vertex attribute pointer and enable VAO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind VAO
+	glBindVertexArray(0);
+}
+
+
+void Structure::draw() const
+{
+	glBindVertexArray(m_VAO);
+	glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, (GLvoid*)0);
+	glBindVertexArray(0);
 }
--- template/VS2013/Structure.h
+++ template/VS2013/Structure.h
@@ -12,14 +12,17 @@ class Structure
 	// height: The height of the structure.
 	static std::pair<std::vector<glm::vec3>, std::vector<GLuint>> computeStructureData(const std::vector<glm::vec2>& baseVertices, const float height);
 
-	std::vector<glm::vec3> m_vertices;
-	std::vector<GLuint> m_indices;
-	GLuint m_VAO;
-	GLuint m_VBO;
-	GLuint m_EBO;
+	
+	std::vector<glm::vec3> m_vertices;	// The structure's vertex coordinates
+	std::vector<GLuint> m_indices;		// The indices to draw the structure as triangles
+	GLuint m_VAO;						// Vertex array object
+	GLuint m_VBO;						// Vertex buffer object
+	GLuint m_EBO;						// Element buffer object
 
-	// Initializes the VAO, VBO, and EBO for drawing the structure.
+	
+	// Generates and initializes the VAO, VBO, and EBO for drawing the structure.
 	void generateBuffers();
+
 
 public:
 
@@ -27,4 +30,8 @@ public:
 	// baseVertices: The vertices of the base polygon.
 	// height: The height of the structure.
 	Structure(const std::vector<glm::vec2>& baseVertices, const float height);
+
+
+	// Draws the structure.
+	void draw() const;
 };
