a58a59ba8b9b25ebe38993fc53a08d26641b4834
chenandy-ca <chenandy.ca@gmail.com>
2016-03-31 14:37:28 -0400

    directionallight class created

---
+++ template/VS2013/DirectionalLight.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Shader.h"
+#include "Camera.h"
+#include "../ThirdParty/glm/gtc/matrix_transform.hpp"
+#include "gtc/matrix_transform.hpp"
+#include "gtc/type_ptr.hpp"
+#include "gtc/constants.hpp"
+#include "glew.h"		// include GL Extension Wrangler
+#include "glfw3.h"  // include GLFW helper library
+#include "glm.hpp"
+
+
+class DirectionalLight{
+public:
+	DirectionalLight(Camera);
+	void UseShader();
+
+private:
+	static Shader shader;
+	static Camera camera;
+
+};
