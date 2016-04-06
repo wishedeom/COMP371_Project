cd4426b437c6c14b051bb13b8378b0d0c3a52365
Wishe Deom <wishedeom@gmail.com>
2016-04-06 19:17:16 -0400

    Adding ReadMe

--- template/Source/main.cpp
+++ template/Source/main.cpp
@@ -1,29 +1,3 @@
-//-------------------------------------------------------------------------------------------------------------------------
-//
-//													COMP 371  -  COMPUTER GRAPHICS
-//
-//														CONCORDIA UNIVERSITY
-//															TERM PROJECT
-//														    MEGA-CITY ONE
-//
-//
-//																Authors:
-//															   Andy Chen
-//															  Michael Deom
-//															 Adrian Gabriel
-//															 Sylvie Truong
-//
-//
-//																PRESENTED
-//															April 7th, 2016
-//																	to
-//														   Prof. Sudhir Mudur
-//																	and
-//														Prof. Charalambos Poullis
-//
-//-------------------------------------------------------------------------------------------------------------------------
-
-
 // Static linking to GLEW
 #define GLEW_STATIC
 
---
+++ template/VS2013/ReadMe.txt
@@ -0,0 +1,25 @@
+//-------------------------------------------------------------------------------------------------------------------------
+//
+//													COMP 371  -  COMPUTER GRAPHICS
+//
+//														CONCORDIA UNIVERSITY
+//															TERM PROJECT
+//														    MEGA-CITY ONE
+//
+//
+//																Authors:
+//																Andy Chen
+//															  Michael Deom
+//															 Adrian Gabriel
+//															 Sylvie Truong
+//
+//
+//																PRESENTED
+//															April 7th, 2016
+//																	to
+//														   Prof. Sudhir Mudur
+//																	and
+//														Prof. Charalambos Poullis
+//
+//-------------------------------------------------------------------------------------------------------------------------
+
