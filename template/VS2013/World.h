75bee93e5b8c9a5428cdba4aea041ba067d1855b
Wishe Deom <wishedeom@gmail.com>
2016-04-06 19:30:12 -0400

    World commented.

--- template/VS2013/World.h
+++ template/VS2013/World.h
@@ -7,14 +7,28 @@
 
 #include "Block.h"
 
+// Represents the entire city.
 class World
 {
-	std::vector<Block> m_blocks;
-	std::vector<glm::vec3> boundingBoxVector;
+
+	std::vector<Block> m_blocks;				// Holds all of the city's blocks
+	std::vector<glm::vec3> boundingBoxVector;	// Holds the extreme coordinates of the city's bounding box
+
 public:
+
+	const int size;	// The size of the city
+
+	// Constructs a city with a given side length, in blocks
 	World(const int aSize);
+
+	// Draws the city.
+	// camera: The camera that is seeing the city.
+	// light: The light that is illuminating the city.
 	void draw(const Camera& camera, const DirectionalLight& light);
+
+	// Returns the bounding box coordinates.
 	std::vector<glm::vec3> getBoundingBoxVector();
+
+	// Returns the blocks of the city.
 	std::vector<Block>& getBlocks();
-	const int size;
 };
